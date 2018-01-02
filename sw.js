d898ef67d955a00e0588ac58f78147f44f9d709e
redback <import1024@foxmail.com>
2018-01-02 22:59:59 +0800

    modify config

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "blog.redback.me",
   "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
